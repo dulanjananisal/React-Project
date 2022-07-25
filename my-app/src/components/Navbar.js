77c76e4daa0b1c61522cf0084d5ca86cca553c5e
dulanjananisal <dulanjananisal67@gmail.com>
2022-07-26 00:21:01 +0530

    Initial Commit

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <nav>
+            <div className="nav-left">
+                <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-1024.png" />
+                <h2>ReactFacts</h2>
+            </div>
+            <div className="nav-right">
+                <h3>React Course - Project-1</h3>
+            </div>
+        </nav>
+    )
+}
---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+function Data(){
+    return(
+        <App />
+    )
+}
+
+ReactDOM.render(<Data />, document.getElementById('root'));
